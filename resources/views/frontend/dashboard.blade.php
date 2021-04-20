fd842eca21498f1851945114747e18cfb98becd0
rafael <rafaelgasperi@clic.com.pa>
2021-04-20 17:31:25 -0500

    bug fix

--- resources/views/frontend/dashboard.blade.php
+++ resources/views/frontend/dashboard.blade.php
@@ -3,27 +3,52 @@
     @include('frontend.partials.title',array('title'=>'Dashboard','subtitle'=>'Bienvenido(a) a GMPCheck'))
 
     <div class="section mt-2">
+        {{--}}
         <div class="row">
-            <div class="col-4">
-                <div class="card text-white bg-light mb-2">
-                    <div class="card-header">Total Equipos</div>
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $data['total_equipos'] }}</h5>
-                        <a href="{{ route('equipos.index') }}" class="btn btn-primary">Ver Equipos</a>
-                    </div>
-                </div>
+            <div class="carousel-single owl-carousel owl-theme">
+                @foreach($data['total_equipos'] as $sub_equipos)
+                    @foreach($sub_equipos['tipos'] as $tipo)
+                        <div class="item">
+                            <div class="card text-white bg-light mb-2">
+                                <div class="card-header">Total Equipos {{ $tipo['tipo'] }}</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $tipo['total'] }}</h5>
+                                    <a href="{{ route('equipos.tipo',array($sub_equipos['name'],$tipo['tipo_id'])) }}"
+                                       class="btn btn-primary">Ver Equipos</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @endforeach
             </div>
-            <div class="col-4">
-                @if(\Sentinel::hasAccess('baterias.index'))
-                    <div class="card text-white bg-light mb-2">
-                        <div class="card-header">Total Baterias</div>
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $data['total_baterias'] }}</h5>
-                            <a href="{{ route('baterias.index') }}" class="btn btn-primary">Ver Baterias</a>
+        </div>
+        {{--}}
+        <div class="row">
+            @foreach($data['total_equipos'] as $sub_equipos)
+                @foreach($sub_equipos['tipos'] as $tipo)
+                    <div class="col-6 col-md-4">
+                        <div class="card text-white bg-light mb-2">
+                            <div class="card-header">Total Equipos {{ $tipo['tipo'] }}</div>
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $tipo['total'] }}</h5>
+                                <a href="{{ route('equipos.tipo',array($sub_equipos['name'],$tipo['tipo_id'])) }}"
+                                   class="btn btn-primary">Ver Equipos</a>
+                            </div>
                         </div>
                     </div>
-                @endif
-            </div>
+                @endforeach
+            @endforeach
+                    <div class="col-6 col-md-4">
+                        @if(\Sentinel::hasAccess('baterias.index'))
+                            <div class="card text-white bg-light mb-2">
+                                <div class="card-header">Total Baterias</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $data['total_baterias'] }}</h5>
+                                    <a href="{{ route('baterias.index') }}" class="btn btn-primary">Ver Baterias</a>
+                                </div>
+                            </div>
+                        @endif
+                    </div>
         </div>
     </div>
 {{--}}
