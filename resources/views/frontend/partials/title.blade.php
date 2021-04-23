708ff9a0c1c473cd031ba2f8a5885212be653192
Johan De Leon <johandeleon@clic.com.pa>
2021-04-23 04:00:41 +0000

    menu dashboard

--- resources/views/frontend/partials/title.blade.php
+++ resources/views/frontend/partials/title.blade.php
@@ -1,8 +1,19 @@
 <div class="appHeader bg-primary text-light">
     <div class="left">
+    @php
+    $routeName = Route::currentRouteName();
+    @endphp
+        @if($routeName=='dashboard')
+        <div class="left">
+        <a href="#" class="headerButton" data-toggle="modal" data-target="#sidebarPanel">
+            <ion-icon name="menu-outline"></ion-icon>
+        </a>
+        </div>
+        @else
         <a href="javascript:;" class="headerButton goBack">
             <ion-icon name="chevron-back-outline" role="img" class="md hydrated" aria-label="chevron back outline"></ion-icon>
         </a>
+        @endif
     </div>
     <div class="pageTitle">{{ $title }}</div>
     <div class="right">
