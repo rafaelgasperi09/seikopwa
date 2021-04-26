02fac07ea3a14a2ce7daab567b006fd5ed965c70
rafael <rafaelgasperi@clic.com.pa>
2021-04-26 07:54:27 -0500

    bug fix

---
+++ resources/lang/es/labels.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Labels
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'activo'  => 'Activo',
+    'activos' => 'Activos',
+    'antena'  => 'Antena',
+    'antena'  => 'Antenas',
+    'color'   => 'Color',
+    'colores' => 'Colores',
+    'componente'  => 'Componente',
+    'componentes'  => 'Componentes',
+    'configuracion'  => 'Configuracion',
+    'configuraciones' => 'Configuraciones',
+    'estructura'  => 'Estructura',
+    'estructuras'  => 'Estructuras',
+    'factura'   => 'Factura',
+    'facturas' => 'Facturas',
+    'cuenta'  => 'Cuenta',
+    'cuentas'  => 'Cuentas',
+    'grupo'  => 'Grupo',
+    'grupos'  => 'Grupos',
+    'modelo'  => 'Modelo',
+    'modelos' => 'Modelos',
+    'lector'  => 'Lector',
+    'lectores'  => 'Lectores',
+    'mantenimiento'   => 'Mantenimiento',
+    'mantenimientos' => 'Mantenimientos',
+    'marca'  => 'Marca',
+    'marcas'  => 'Marcas',
+    'modelo'  => 'Modelo',
+    'modelos' => 'Modelos',
+    'proveedor'  => 'Proveedor',
+    'proveedores'  => 'Proveedores',
+    'region'   => 'Region',
+    'regiones' => 'Regiones',
+    'subgrupo'  => 'Sub grupo',
+    'subgrupos'  => 'Sub grupos',
+    'subregion'  => 'Ubicación',
+    'subregiones'  => 'Ubicaciones',
+    'rol'    => 'Rol',
+    'roles'  => 'Roles',
+    'responsable'  => 'Responsable',
+    'responsables'  => 'Responsables',
+];
