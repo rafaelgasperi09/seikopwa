d8ee1091fd8f2194d8cbe1a926135f1ce0865a6d
rafael <rafaelgasperi@clic.com.pa>
2021-12-29 15:23:41 -0500

    bug fix

--- resources/views/frontend/dashboard.blade.php
+++ resources/views/frontend/dashboard.blade.php
@@ -257,15 +257,15 @@
                     <div class="card-body">
                     @if(count($data['serv_tec_10']) and $count=1)
                         @foreach($data['serv_tec_10'] as $st10)
-
+                           @if($st10->equipo())
                            <a href="{{ route('equipos.detail',array('id'=>$st10->equipo()->id)) }}?show=rows&tab=3" class="chip  chip-media ml-05 mb-05"  style="width:100%">
                                 <i class="chip-icon bg-{!!getStatusBgColor($st10->estatus)!!}">
                                     {{$st10->estatus}}
                                 </i>
                                 <span class="chip-label">{{$st10->equipo()->numero_parte}}</span>
                             </a>
+                            @endif
                         @endforeach
-
                     @endif
                     </div>
                 </div>  <br/>
