e2e2efa54ae752a667311cf8bd44b44bef685992
rafael <rafaelgasperi@clic.com.pa>
2021-12-29 15:22:46 -0500

    bug fix

--- resources/views/frontend/dashboard.blade.php
+++ resources/views/frontend/dashboard.blade.php
@@ -190,38 +190,33 @@
                     <div class="card-body">
                     @if(count($data['serv_tec_pr']))
                         @foreach($data['serv_tec_pr'] as $stpr)
-                        <a href="{{ route('equipos.detail',array('id'=>$stpr->equipo()->id)) }}?show=rows&tab=3"  class="chip chip-danger chip-media ml-05 mb-05" style="padding:18px;width:100%">
-                            <i class="chip-icon">
-                                Ir
-                            </i>
-                            @php
-                                $fecha_sta=$stpr->estatusHistory()->orderBy('created_at','desc')->first()->created_at;
-                                $date1 = new DateTime($fecha_sta);
-                                $date2 = new DateTime(date('Y-m-d h:i:s'));
-                                $diff = $date1->diff($date2);
-                                // will output 2 days
-
-                                $transcurrido='';
-                                if($diff->d)
-                                    $transcurrido=$diff->format('%dd %hh %im');
-                                else
-                                     $transcurrido=$diff->format('%hh %im');
-
-                            @endphp
-                            <span class="chip-label">{{$stpr->equipo()->numero_parte}} </span>
-                            <div  class="fecha pull-right" >
-                                <span title="Fecha de Inicio">
-                                     {{transletaDate($fecha_sta)}}
-                                </span><br/>
-                                <span title="Tiempo transcurrido">
-                                     {{$transcurrido}}
-                                </span>
-                            </div>
-
-
-                        </a>
+                            @if($stpr->equipo())
+                                <a href="{{ route('equipos.detail',array('id'=>$stpr->equipo()->id)) }}?show=rows&tab=3"  class="chip chip-danger chip-media ml-05 mb-05" style="padding:18px;width:100%">
+                                    <i class="chip-icon"> Ir</i>
+                                    @php
+                                        $fecha_sta=$stpr->estatusHistory()->orderBy('created_at','desc')->first()->created_at;
+                                        $date1 = new DateTime($fecha_sta);
+                                        $date2 = new DateTime(date('Y-m-d h:i:s'));
+                                        $diff = $date1->diff($date2);
+                                        // will output 2 days
+                                        $transcurrido='';
+                                        if($diff->d)
+                                            $transcurrido=$diff->format('%dd %hh %im');
+                                        else
+                                             $transcurrido=$diff->format('%hh %im');
+                                    @endphp
+                                    <span class="chip-label">{{$stpr->equipo()->numero_parte}} </span>
+                                    <div  class="fecha pull-right" >
+                                        <span title="Fecha de Inicio">
+                                             {{transletaDate($fecha_sta)}}
+                                        </span><br/>
+                                        <span title="Tiempo transcurrido">
+                                             {{$transcurrido}}
+                                        </span>
+                                    </div>
+                                </a>
+                        @endif
                         @endforeach
-
                     @endif
                     </div>
                 </div> <br/>
