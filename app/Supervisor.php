bf804ca97e18b193df416001d97f689b89e8eef4
jdeleon2305 <johandeleon@clic.com.pa>
2023-06-09 12:53:13 +0000

    chmod

--- app/Supervisor.php
+++ app/Supervisor.php
@@ -3,13 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Supervisor extends BaseModel
 {
     protected $table = 'supervisores';
-
+    use Notifiable;
     
    public function getFullNameAttribute() {
         return $this->first_name.' '.$this->last_name; //Change the format to whichever you desire
     }
 }
+
--- app/SupervisorCli.php
+++ app/SupervisorCli.php
@@ -3,13 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class SupervisorCli extends BaseModel
 {
     protected $table = 'supervisores_cli';
-
+    use Notifiable;
     
    public function getFullNameAttribute() {
         return $this->first_name.' '.$this->last_name; //Change the format to whichever you desire
     }
 }
+
