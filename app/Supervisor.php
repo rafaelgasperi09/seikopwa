fb761a9b99dcb1b2d479f2ee3c215c8b1f892df9
Johan De Leon <johandeleon@clic.com.pa>
2023-06-08 22:44:06 -0500

    notifiable

--- app/Supervisor.php
+++ app/Supervisor.php
@@ -3,11 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Supervisor extends BaseModel
 {
     protected $table = 'supervisores';
-
+    use Notifiable;
     
    public function getFullNameAttribute() {
         return $this->first_name.' '.$this->last_name; //Change the format to whichever you desire
--- app/SupervisorCli.php
+++ app/SupervisorCli.php
@@ -3,11 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class SupervisorCli extends BaseModel
 {
     protected $table = 'supervisores_cli';
-
+    use Notifiable;
     
    public function getFullNameAttribute() {
         return $this->first_name.' '.$this->last_name; //Change the format to whichever you desire
