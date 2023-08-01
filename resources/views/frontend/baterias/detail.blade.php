41d216281109bb1e316b000607a64daf93242343
Johan De Leon <johandeleon@clic.com.pa>
2023-07-31 22:30:43 -0500

    fix

--- resources/views/frontend/baterias/detail.blade.php
+++ resources/views/frontend/baterias/detail.blade.php
@@ -189,7 +189,7 @@
             "processing": true,
             "serverSide": true,
             "autoWidth": false,
-            "ajax": "{{ route('baterias.datatable',array('id'=>$data->id)) }}",
+            "ajax": "{{ secure_url('/equipos/reportes_datatable',array('id'=>$data->id)) }}",
             "columns":[
                 {data:'accion'},
                 {data:'fecha'},
