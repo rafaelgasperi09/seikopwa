018a24534487f219e98ac698babc26deed9bb902
johandeleon <jdeleon@hcaglobal.com>
2023-08-03 17:34:33 -0500

    canvas reporte

--- resources/views/frontend/equipos/create_control_alquiler.blade.php
+++ resources/views/frontend/equipos/create_control_alquiler.blade.php
@@ -100,6 +100,20 @@
             {{ Form::hidden('componente_id',$data->id) }}
             {{ Form::hidden('formulario_id',$formulario->id) }}
             @include('frontend.partials.form',array('formulario'=>$formulario))
+            <div class="section full mt-2 mb-2" id="seccion8">
+                <div class="section-title"></div>
+                <div class="wide-block pb-1 pt-2">
+                    <div class="row">
+                        <div class="form-group boxed col-12">
+                            <div class="input-wrapper text-center">
+                            <label class="label" for="comentarios">Marque con un gancho</label> 
+                            <canvas height="800px" width="800px" id="imgmc"></canvas>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                </div>                       
             <div class="modal-footer">
                 @include('frontend.partials.btnSubmit')
             </div>
@@ -107,13 +121,31 @@
         </div>
     </div>
 <script>
+    var canvas = document.getElementById("imgmc");
+var ctx = canvas.getContext("2d");
+
+var img = new Image();
+img.src = "{{url('images/montacargas/800/combustion.png')}}";
+
+// Importante el onload
+img.onload = function(){
+	ctx.drawImage(img, 0, 0);
+}
+    
+    
+     $(document).on('click','#imgmc',function(event){
+        var coordenadas = $("#imgmc").offset();
+        
+    console.log("Y: " + coordenadas.top + " X: " + coordenadas.left);
+    console.log("Y: " + event.clientX + " X: " + event.clientY);
+    });
     $('#fecha').val('{{date('Y-m-d')}}');
     $('#fecha').attr('readonly','readonly');
     $('#hora_entrada').val('{{date('H:i')}}');
     $('#hora_entrada').attr('readonly','readonly');
     @if(current_user()->isOnGroup('tecnico'))
     $('#typehead_tecnico_id').val('{{current_user()->full_name}}');
-    $('#tecnico_id').val('{{current_user()->id}}');
+    $('#tecnico_id').val('{{current_user()->id}}');   
     @endif
 
 </script>
