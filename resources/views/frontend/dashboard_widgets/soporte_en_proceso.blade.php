5153fd41735399ef4d7bffef29259e775f454e89
johandeleon <jdeleon@hcaglobal.com>
2023-08-16 13:20:54 -0500

    fix enlace

--- resources/views/frontend/dashboard_widgets/soporte_en_proceso.blade.php
+++ resources/views/frontend/dashboard_widgets/soporte_en_proceso.blade.php
@@ -77,7 +77,8 @@
                         </div>
                         @foreach($data['serv_tec_pr']->where('cliente_id',$gstpri->cliente_id)->where('equipo_status','I') as $stpri)
                             @if($stpri->equipo())
-                                <a href="{{ route('equipos.detail',array('id'=>$stpr->equipo_id)) }}?show=rows&tab=3"  
+
+                                <a href="{{ route('equipos.detail',array('id'=>$stpri->equipo_id)) }}?show=rows&tab=3&xxx=tttt"  
                                 class="chip chip-media ml-05 mb-05 stprlist_i stpri{{$gstpri->cliente_id}}" style="padding:18px;width:98%; @if($l!=0)  display:none; @endif">
                                     
                                     <i class="chip-icon bg-{!!getStatusBgColor($stpri->estatus)!!}">
