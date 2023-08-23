a1f8c94e1e98a7aea1e9bce007cd963975b59ad3
johandeleon <jdeleon@hcaglobal.com>
2023-08-23 15:49:05 -0500

    fix users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,6 +46,7 @@ class UserController extends Controller
                     ->when($where<>'()',function($q) use ($where) {
                         $q->whereRaw($where);
                     })
+                    ->selectRaw('users.*')
                     ->paginate(10);
 
 
