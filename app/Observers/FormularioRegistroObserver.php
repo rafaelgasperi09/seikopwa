d8deb980036f814597be7885b9a81cc58ed93746
johandeleon <jdeleon@hcaglobal.com>
2024-01-30 15:54:23 -0500

    fix error

--- app/Observers/FormularioRegistroObserver.php
+++ app/Observers/FormularioRegistroObserver.php
@@ -212,16 +212,18 @@ class FormularioRegistroObserver
                     //NOTIFICA SUPERVISORES CLIENTES
                   
 
-                    $notificadosCli = User::whereHas('roles',function ($q){
-                                            $q->whereIn('role_id',[3,13]);
-                                        })
-                                        ->whereRaw("(crm_clientes_id ='$equipo->cliente_id' or crm_clientes_id like '%,$equipo->cliente_id%' or crm_clientes_id like '%$equipo->cliente_id,%')")
-                                        ->when(!empty($supervisor_id),function($q) use($supervisor_id){
-                                            $q->where('id',$supervisor_id);
-                                        })    
-                                        ->get();
-                    
+                   
                     if($campo->formulario->nombre=='form_montacarga_daily_check'){
+                        
+                        $notificadosCli = User::whereHas('roles',function ($q){
+                            $q->whereIn('role_id',[3,13]);
+                        })
+                        ->whereRaw("(crm_clientes_id ='$equipo->cliente_id' or crm_clientes_id like '%,$equipo->cliente_id%' or crm_clientes_id like '%$equipo->cliente_id,%')")
+                        ->when(!empty($supervisor_id),function($q) use($supervisor_id){
+                            $q->where('id',$supervisor_id);
+                        })    
+                        ->get();
+    
                         foreach ($notificadosCli as $u){
                             notifica($u,(new DailyCheckIsFinnish($formularioRegistro,$u,$notificadosCli))->delay($when));
                             if(env_local()){
