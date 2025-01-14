b11a8e2a76723fa818cdd74613c1e5a308ffa48b
johandeleon <jdeleon@hcaglobal.com>
2025-01-14 11:43:09 -0500

    search false

--- resources/views/frontend/equipos/reportes.blade.php
+++ resources/views/frontend/equipos/reportes.blade.php
@@ -61,6 +61,7 @@
                 "autoWidth": false,
                 "order": [[ 0, "desc" ]],
                 "processing": true,
+                "searching": false
                 "serverSide": true,
                 "ajax": "{{url('equipos/reportes_datatable') }}?"+parameters,
                 "columns":[
