c5c9cd83a1c37acd2d612f1265fc835b6459aa80
johandeleon <jdeleon@hcaglobal.com>
2025-02-24 10:38:04 -0500

    logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -97,7 +97,7 @@ class LoginController extends Controller
         Sentinel::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect(route('login'))->withHeaders([
+        return redirect('/')->withHeaders([
             'Cache-Control' => 'no-store, no-cache, must-revalidate, max-age=0',
             'Pragma' => 'no-cache',
             'Expires' => 'Sat, 01 Jan 2000 00:00:00 GMT'
