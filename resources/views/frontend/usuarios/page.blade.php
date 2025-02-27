b83d1fbe5795489cb3e3e88a4b63883ab5a8aebd
johandeleon <jdeleon@hcaglobal.com>
2025-02-27 15:06:00 -0500

    activa

--- resources/views/frontend/usuarios/page.blade.php
+++ resources/views/frontend/usuarios/page.blade.php
@@ -54,7 +54,7 @@
                 </a>
             </li>
             @endif
-            @if(\Sentinel::hasAccess('usuarios.delete') && !$dato->isOnGroup('programador'))
+            @if(\Sentinel::hasAccess('usuarios.delete') && !$dato->isOnGroup('programador') and $dato->completed==1)
             <li>
                 <a href="#" class="item" data-toggle="modal" data-target="#deleteModal"
                 data-action="{{ route('usuarios.delete',$dato->id) }}" data-message="Estas seguro que deseas desactivar a este usuario ?">
@@ -63,6 +63,20 @@
                     </div>
                     <div class="in">
                         <div>Desactivar</div>
+                    </div>
+                </a>
+
+            </li>
+            @endif
+            @if(\Sentinel::hasAccess('usuarios.delete') && !$dato->isOnGroup('programador') and $dato->completed=='')
+            <li>
+                <a href="{{ route( 'usuarios.activar',$dato->id) }}" class="item" 
+                data-action="{{ route('usuarios.delete',$dato->id) }}" data-message="Estas seguro que deseas desactivar a este usuario ?">
+                    <div  class="icon-box bg-success">
+                        <ion-icon name="close-outline" role="img" class="md hydrated" aria-label="videocam outline"></ion-icon>
+                    </div>
+                    <div class="in">
+                        <div>Activar</div>
                     </div>
                 </a>
 
