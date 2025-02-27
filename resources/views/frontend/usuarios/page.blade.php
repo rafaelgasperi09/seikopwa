562e4af31a5d46080de236429113768ac548f8af
johandeleon <jdeleon@hcaglobal.com>
2025-02-27 15:10:42 -0500

    activa

--- resources/views/frontend/usuarios/page.blade.php
+++ resources/views/frontend/usuarios/page.blade.php
@@ -59,7 +59,7 @@
                 <a href="#" class="item" data-toggle="modal" data-target="#deleteModal"
                 data-action="{{ route('usuarios.delete',$dato->id) }}" data-message="Estas seguro que deseas desactivar a este usuario ?">
                     <div  class="icon-box bg-danger">
-                        <ion-icon name="checkmark-circle-outline" role="img" class="md hydrated" aria-label="videocam outline"></ion-icon>
+                    <ion-icon name="close-outline" role="img" class="md hydrated" aria-label="videocam outline"></ion-icon>
                     </div>
                     <div class="in">
                         <div>Desactivar</div>
@@ -72,7 +72,7 @@
             <li>
                 <a href="{{ route( 'usuarios.activar',$dato->id) }}" class="item" >
                     <div  class="icon-box bg-success">
-                        <ion-icon name="close-outline" role="img" class="md hydrated" aria-label="videocam outline"></ion-icon>
+                        <ion-icon name="checkmark-circle-outline" role="img" class="md hydrated" aria-label="videocam outline"></ion-icon>
                     </div>
                     <div class="in">
                         <div>Activar</div>
