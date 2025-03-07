f6e939c104d21d93e68985422ffe4955b01a795b
johandeleon <jdeleon@hcaglobal.com>
2025-03-07 14:22:15 -0500

    order default

--- app/Marca.php
+++ app/Marca.php
@@ -3,11 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Marca extends BaseModel
 {
 
     protected $table = 'marcas';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('ordenPorDefecto', function (Builder $builder) {
+            $builder->orderBy('display_name', 'asc'); // Cambia 'nombre_columna' y 'asc' según necesites
+        });
+    }
 
     public function equipos(){
         return $this->hasMany(Equipo::class);
