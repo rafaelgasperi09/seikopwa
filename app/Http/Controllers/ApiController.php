b1bce62b87f9b57fb5ca4e2dcb82f541a7625b6f
johandeleon <jdeleon@hcaglobal.com>
2025-03-21 15:18:46 -0500

    test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -296,34 +296,34 @@ class ApiController extends Controller
 
                         $cliente=$gsta->cliente();
                         $nombre='';
-                        if($cliente)
+                        if($cliente){
                             $nombre=$cliente->nombre;
-                        $result5.='<div class="chip chip-danger chip-media ml-05 mb-05" style="width:100%;margin-top:15px !important;font-size:16px">
-                            <span class="chip-label ">'.$nombre.' </span>
-                            <i class="chip-icon abrirsta"  id="sta'.$gsta->cliente_id.'" >
-                                <span class=" pull-right flechasta flechasta'.$gsta->cliente_id.'"title="Ver mas">';
-                        if($k==0 )
-                            $result5.='<ion-icon name="chevron-down-outline"></ion-icon></span>';
-                        else
-                            $result5.='<ion-icon name="chevron-up-outline"></ion-icon></span>';
-                            $result5.='</i></div>';
-
-                        foreach($data['serv_tec_pi_a']->where('cliente_id',$gsta->cliente_id) as $sta){
-                        
-                            if($k<>0 and !$abierta0)
-                                $display='display:none';
-                            $result5.='<a href="'.route('equipos.detail',array('id'=>$sta->equipo()->id)) .'?show=rows&tab=3"
-                            class="chip chip-danger chip-media ml-05 mb-05 stalist sta'.$gsta->cliente_id.'" style="width:98%;'.$display.'">
-                                <i class="chip-icon">
-                                    Ir
-                                </i>
-                                <span class="chip-label">'.$sta->equipo()->numero_parte.' </span>
-                                <span class="fecha pull-right" title="Fecha de asignacion de tecnico">
-                                        '.transletaDate($sta->estatusHistory()->orderBy('created_at','desc')->first()->created_at,true,'').'
-                                </span>
-                            </a>';
-                        }
-                    
+                            $result5.='<div class="chip chip-danger chip-media ml-05 mb-05" style="width:100%;margin-top:15px !important;font-size:16px">
+                                <span class="chip-label ">'.$nombre.' </span>
+                                <i class="chip-icon abrirsta"  id="sta'.$gsta->cliente_id.'" >
+                                    <span class=" pull-right flechasta flechasta'.$gsta->cliente_id.'"title="Ver mas">';
+                            if($k==0 )
+                                $result5.='<ion-icon name="chevron-down-outline"></ion-icon></span>';
+                            else
+                                $result5.='<ion-icon name="chevron-up-outline"></ion-icon></span>';
+                                $result5.='</i></div>';
+    
+                            foreach($data['serv_tec_pi_a']->where('cliente_id',$gsta->cliente_id) as $sta){
+                            
+                                if($k<>0 and !$abierta0)
+                                    $display='display:none';
+                                $result5.='<a href="'.route('equipos.detail',array('id'=>$sta->equipo()->id)) .'?show=rows&tab=3"
+                                class="chip chip-danger chip-media ml-05 mb-05 stalist sta'.$gsta->cliente_id.'" style="width:98%;'.$display.'">
+                                    <i class="chip-icon">
+                                        Ir
+                                    </i>
+                                    <span class="chip-label">'.$sta->equipo()->numero_parte.' </span>
+                                    <span class="fecha pull-right" title="Fecha de asignacion de tecnico">
+                                            '.transletaDate($sta->estatusHistory()->orderBy('created_at','desc')->first()->created_at,true,'').'
+                                    </span>
+                                </a>';
+                            }
+                        }                    
                 }
             }
 
