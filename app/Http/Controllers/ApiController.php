bc24e4e5a78c1169e712f4bb5c4544bebbc7a3bb
johandeleon <jdeleon@hcaglobal.com>
2025-03-27 09:40:32 -0500

    group

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -181,7 +181,7 @@ class ApiController extends Controller
                     foreach($data['mant_prev'] as $k=>$mp){
                         $cliente=Cliente::find($mp->cliente_id);
                         if($cliente){
-                            if(!isset($clientes_list[$mp->cliente_id])){
+                            if(!isset($clientes_list[$mp->cliente_id]) and $clientes_list[$mp->cliente_id]==0){
                                 $result3.='<div class="chip chip-warning chip-media ml-05 mb-05" style="width:100%;margin-top:15px !important;font-size:16px">
                                 <span class="chip-label">
                                 '.$cliente->nombre.' 
@@ -196,9 +196,9 @@ class ApiController extends Controller
                                             $result3.='</i>
                                         </div>';
 
-                                
+                                $clientes_list[$mp->cliente_id]=0;
                                 }else{
-                                    $clientes_list[$mp->cliente_id]=$mp->cliente_id;
+                                    $clientes_list[$mp->cliente_id]++;
                                 }
                                 $result3.='<a href="'.route('equipos.detail',array('id'=>$mp->equipo_id)) .'?show=rows&tab=2"  class="chip chip-warning chip-media ml-05 mb-05 gmplist gmp'.$mp->cliente_id.'" style="width:98%;';
                                 
