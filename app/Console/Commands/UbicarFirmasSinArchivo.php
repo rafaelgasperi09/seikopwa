01a37b0285c6a724b1af67cccc8c7a26ea3aaaa9
jdeleon2305 <jdeleon@entorno-virtual.com>
2025-04-22 11:09:22 -0500

    comando

--- app/Console/Commands/UbicarFirmasSinArchivo.php
+++ app/Console/Commands/UbicarFirmasSinArchivo.php
@@ -58,7 +58,7 @@ class UbicarFirmasSinArchivo extends Command
                 $filename[count($filename)-1]='';
                 $filename=implode('_',$filename);
                 $file=FormularioData::where('tipo','firma')->whereNotNull('file_path')->whereRaw("LENGTH(valor)>3 and valor like '$filename%'")->first();
-                //copy( storage_path('app/public/firmas/'.$file->valor), storage_path('app/public/firmas/'.$d->valor));
+                copy( storage_path('app/public/firmas/'.$file->valor), storage_path('app/public/firmas/'.$d->valor));
                 $this->info("------------------NO SE ENCONTRO PARA EL REPORTE ".$d->formulario_registro_id."-------------------");
                 dd($d->formulario_registro_id);
             }
